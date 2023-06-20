01e55255fc5f2d2cc5fe796daa33f3ee393fd484
francisco <franciscomeza.1234@gmail.com>
2023-06-20 11:37:12 -0400

    firstcommit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function borde() {
+    let image = document.querySelector("img");
+    if (image.style.border === "2px solid red") {
+      image.style.border = "none";
+    } else {
+      image.style.border = "2px solid red";
+    }
+  }
